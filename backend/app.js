98fba0c845aabf748dbac836cc1e9368b17b7925
harry <enesdyaz@gmail.com>
2020-11-29 22:38:27 +0800

    bolier

---
+++ backend/app.js
@@ -0,0 +1,91 @@
+//------------------------------------------------//
+// BACKEND/FRONT PORT
+//------------------------------------------------//
+const backPort = '3085'
+const frontPort = '3000'
+
+
+const express = require('express')
+const db = require('./models')
+const cors = require('cors')
+const morgan = require('morgan')
+
+//------------------------------------------------//
+// ROUTER REQUIRE
+//------------------------------------------------//
+const userRouter = require('./routes/user')
+// const widgetRouter= require('./routes/widget')
+// const imageRouter = require('./routes/image')
+// const databaseRouter = require('./routes/database')
+// const postRouter = require('./routes/post')
+// const loadPostRouter = require('./routes/loadPost')
+
+//------------------------------------------------//
+// PASSPORT MODULE
+//------------------------------------------------//
+const passport = require('passport')
+const passportConfig = require('./passport')
+const session = require('express-session')
+const cookie = require('cookie-parser')
+const app = express()
+app.use(morgan('dev'))
+
+db.sequelize.sync( ) // db 실행 {force:true}
+
+
+app.use(express.json()) // express 가 json을 받기 위함
+app.use(express.urlencoded( {extended: false})) 
+// passport
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'ns747800',
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    }
+}))
+app.use(cookie('ns747800'))
+app.use(passport.initialize())
+app.use(passport.session())
+passportConfig()
+
+
+
+//------------------------------------------------//
+// CORS FRONTEND PORT
+//------------------------------------------------//
+
+app.use(cors({
+    origin: `http://localhost:${frontPort}`,
+    credentials: true
+}))   
+
+//------------------------------------------------//
+// IMAGE STATIC FILES
+//------------------------------------------------//
+
+app.use('/', express.static('uploads'));
+
+
+//------------------------------------------------//
+// ROUTER API
+//------------------------------------------------//
+
+app.get('/', (req,res)=>{ res.send('hello wizeto') })
+app.use('/user', userRouter)  
+// app.use('/image', imageRouter)
+// app.use('/widget', widgetRouter) 
+
+// app.use('/database', databaseRouter)
+// app.use('/post', postRouter)
+// app.use('/loadpost', loadPostRouter)
+
+
+
+
+
+//------------------------------------------------//
+// LISTEN
+//------------------------------------------------//
+app.listen(backPort,()=>{ console.log(`YES backend server is ${backPort}`) })
---
+++ backend/models/index.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+
+
+//------------------------------------------------//
+// ADD MODELS 
+//------------------------------------------------//
+
+db.User = require('./User') (sequelize, Sequelize)  
+// db.Image = require('./Image') (sequelize, Sequelize)  
+// db.Post = require('./Post')(sequelize, Sequelize)  
+// db.Comment = require('./Comment')(sequelize, Sequelize)  
+// db.Hashtag = require('./hashtag')(sequelize, Sequelize)  
+
+
+// db.MainFormContent = require('./MainFormContent')(sequelize, Sequelize)
+// db.Database = require('./Database')(sequelize, Sequelize)
+
+
+
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
+
